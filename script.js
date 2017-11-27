8e5d01a92b1ba9703cbb5acc2942efe35cff87fe
Devendra Rajput <devenrajput12@gmail.com>
2017-11-27 18:41:10 +0530

    adding two files

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ var app = angular.module("app", []);
 app.controller("HelloController", function($scope){
 	$scope.message = "Hello World";
 });
+
+
+app.controller("getName",function($scope){
+  $scope.firstName = "";
+  $scope.lastName = "";
+    return $scope.firstName + " " + $scope.lastName;
+});
+
+
+app.controller("usingFilters",function($scope){
+	var data = [
+		{name: "Devendra", dateOfBirth: new Date("19 February, 1992"), gender: "Male", companyName: "Cyber Group"},
+		{name: "Vishal", dateOfBirth: new Date("October 19, 1992"), gender: "Male", companyName: "Cyber Group"},
+		{name: "Lagnesh", dateOfBirth: new Date("July 22, 1992"), gender: "Male", companyName: "Cyber Group"},
+		{name: "Shantanu", dateOfBirth: new Date("March 19, 1988"), gender: "Male", companyName: "Cyber Group"},
+	];
+
+	$scope.data = data;
+});
